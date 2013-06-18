be2bbf1bd9217ffa3ab563ca782188841d82de86
Kosuke Asami <tfortress58@gmail.com>
2013-06-18 19:35:58 +0900

    Extract author from EPUB file

--- lib/epub-searcher/epub-document.rb
+++ lib/epub-searcher/epub-document.rb
@@ -1,5 +1,10 @@
 module EPUBSearcher
   class EPUBDocument
+    def extract_author(epub_book)
+      metadata = epub_book.metadata
+      return metadata.author
+    end
+
     def extract_title(epub_book)
       metadata = epub_book.metadata
       return metadata.title
--- test/test-epub-document.rb
+++ test/test-epub-document.rb
@@ -6,6 +6,12 @@ require 'epub/parser'
 require 'epub-searcher/epub-document'
 
 class TestEPUBDocument < Test::Unit::TestCase
+  def text_extract_author
+    epub_book = EPUB::Parser.parse(fixture_path('groonga.epub'))
+    document = EPUBSearcher::EPUBDocument.new
+    assert_equal("groonga", document.extract_author(epub_book))
+  end
+
   def test_extract_title
     epub_book = EPUB::Parser.parse(fixture_path('groonga.epub'))
     document = EPUBSearcher::EPUBDocument.new
