b668ee1a7f9707297b3487ede5be2b569fbf54fd
Kosuke Asami <tfortress58@gmail.com>
2013-06-20 11:35:43 +0900

    Emphasize the number of fixtures may not be 1
    @document->
    @document_1

--- test/test-epub-document.rb
+++ test/test-epub-document.rb
@@ -10,23 +10,23 @@ class TestEPUBDocument < Test::Unit::TestCase
     # groonga_doc_all.epub ... spine を一つしか含まない EPUB ファイル
     #                          本文は groonga ドキュメント 1 章 が全て入っている
     epub_book = EPUB::Parser.parse(fixture_path('groonga_doc_all.epub'))
-    @document = EPUBSearcher::EPUBDocument.new(epub_book)
+    @document_1 = EPUBSearcher::EPUBDocument.new(epub_book)
   end
 
   def test_extract_contributors
-    assert_equal([], @document.extract_contributors)
+    assert_equal([], @document_1.extract_contributors)
   end
 
   def test_extract_creators
-    assert_equal(["groonga"], @document.extract_creators)
+    assert_equal(["groonga"], @document_1.extract_creators)
   end
 
   def test_extract_title
-    assert_equal("groongaについて", @document.extract_title)
+    assert_equal("groongaについて", @document_1.extract_title)
   end
 
   def test_extract_xhtml_spine
-    assert_equal(["OEBPS/item0001.xhtml"], @document.extract_xhtml_spine)
+    assert_equal(["OEBPS/item0001.xhtml"], @document_1.extract_xhtml_spine)
   end
 
   private
