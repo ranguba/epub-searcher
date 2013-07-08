54e6aa8ab978c0bcfd95569181ccb591dd20279d
Kosuke Asami <tfortress58@gmail.com>
2013-07-08 15:41:06 +0900

    Check a query before search

--- app/controllers/main-controller.rb
+++ app/controllers/main-controller.rb
@@ -6,12 +6,16 @@ Epub::App.controllers do
   get :index do
     query_words = params[:q]
 
+    right_query = query_words && !query_words.empty?
+    if right_query
     hit_records = search_from_groonga(query_words)
 
     @results = Array.new
     hit_records.each do |record|
       @results << record
     end
+    end
+
     render 'index'
   end
 
