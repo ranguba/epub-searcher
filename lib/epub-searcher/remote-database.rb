1920e4341c35b44c02535298a8e890d39557d254
Kosuke Asami <tfortress58@gmail.com>
2013-07-09 11:17:19 +0900

    Provide a client class that setup groonga server and load records to it

---
+++ lib/epub-searcher/remote-database.rb
@@ -0,0 +1,74 @@
+require 'groonga/client'
+
+module EPUBSearcher
+  class RemoteDatabase
+    attr_reader :client
+
+    def initialize(options={})
+      @client = Groonga::Client.open(options)
+    end
+
+    def close
+      @client.close
+    end
+
+    def load_records(epub_documents)
+      groonga_load_records(epub_documents)
+    end
+
+    def setup_database
+      groonga_setup_db_books
+      groonga_setup_db_terms
+    end
+
+    private
+    def groonga_setup_db_books
+      @client.table_create(:name => :Books, :flags => 'TABLE_NO_KEY')
+      ['author', 'file_path', 'title'].each do |column|
+        @client.column_create(
+          :table => :Books,
+          :name => column,
+          :flags => 'COLUMN_SCALAR',
+          :type => :ShortText,
+        )
+      end
+      @client.column_create(
+        :table => :Books,
+        :name => 'main_text',
+        :flags => 'COLUMN_SCALAR',
+        :type => :LongText,
+      )
+    end
+
+    def groonga_setup_db_terms
+      @client.table_create(
+        :name => :Terms,
+        :flags => 'TABLE_PAT_KEY',
+        :type => :ShortText,
+        :default_tokenizer => :TokenBigram,
+        :normalizer => :NormalizerAuto,
+      )
+      ['author', 'main_text', 'title'].each do |column|
+        @client.column_create(
+          :table => :Terms,
+          :name => 'entries_' + column + '_index',
+          :flags => 'COLUMN_INDEX|WITH_POSITION',
+          :type => :Books,
+          :source => column,
+        )
+      end
+    end
+
+    def groonga_load_records(epub_documents)
+      records = epub_documents.map do |epub_document|
+        {
+          :author => epub_document.extract_creators.join(' '),
+          :main_text => epub_document.extract_main_text,
+          :title => epub_document.extract_title,
+          :file_path => epub_document.file_path,
+        }
+      end
+      @client.load(:table => :Books, :values => records.to_json)
+    end
+  end
+end
---
+++ test/epub-searcher/remote_database_test.rb
@@ -0,0 +1,84 @@
+require 'epub-searcher/remote-database'
+
+class TestRemoteDatabase < Test::Unit::TestCase
+  class TestSetup < self
+    def setup
+      @database = EPUBSearcher::RemoteDatabase.new
+    end
+
+    def teardown
+      @database.close
+    end
+
+    def test_setup_database
+      expected_table_create_params = [
+        {:name=>:Books, :flags=>'TABLE_NO_KEY'},
+        {:name=>:Terms, :flags=>'TABLE_PAT_KEY', :type=>:ShortText, :default_tokenizer=>:TokenBigram, :normalizer=>:NormalizerAuto},
+      ]
+
+      expected_column_create_params = [
+        {:table=>:Books, :name=>'author', :flags=>'COLUMN_SCALAR', :type=>:ShortText},
+        {:table=>:Books, :name=>'file_path', :flags=>'COLUMN_SCALAR', :type=>:ShortText},
+        {:table=>:Books, :name=>'title', :flags=>'COLUMN_SCALAR', :type=>:ShortText},
+        {:table=>:Books, :name=>'main_text', :flags=>'COLUMN_SCALAR', :type=>:LongText},
+        {:table=>:Terms, :name=>'entries_author_index', :flags=>'COLUMN_INDEX|WITH_POSITION', :type=>:Books, :source=>'author'},
+        {:table=>:Terms, :name=>'entries_main_text_index', :flags=>'COLUMN_INDEX|WITH_POSITION', :type=>:Books, :source=>'main_text'},
+        {:table=>:Terms, :name=>'entries_title_index', :flags=>'COLUMN_INDEX|WITH_POSITION', :type=>:Books, :source=>'title'},
+      ]
+
+      @database.client
+        .expects(:table_create)
+        .times(expected_table_create_params.size)
+        .with do |params|
+        expected_table_create_params.include?(params)
+      end
+
+      @database.client
+        .expects(:column_create)
+        .times(expected_column_create_params.size)
+        .with do |params|
+        expected_column_create_params.include?(params)
+      end
+
+      @database.setup_database
+    end
+  end
+
+  class TestRecords < self
+    def setup
+      @database = EPUBSearcher::RemoteDatabase.new
+    end
+
+    def teardown
+      @database.close
+    end
+
+    def test_load_records
+      epub_paths = [
+        'empty_contributors_single_spine.epub',
+        'single_contributors_multi_spine.epub',
+        'multi_contributors_multi_spine.epub',
+      ]
+
+      documents = epub_paths.collect do |path|
+        document = EPUBSearcher::EPUBDocument.open(fixture_path(path))
+        document
+      end
+
+      expected = File.read(fixture_path('load_records_params_expected.txt'))
+      @database.client
+        .expects(:load)
+        .once
+        .with do |params|
+        expected == params.to_s
+      end
+
+      @database.load_records(documents)
+    end
+  end
+
+  private
+  def fixture_path(basename)
+    File.join(__dir__, 'fixtures', basename)
+  end
+end
