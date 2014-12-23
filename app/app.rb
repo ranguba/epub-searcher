2a6010cd6a465593aef4770f877429b18438e993
KITAITI Makoto <KitaitiMakoto@gmail.com>
2014-12-23 16:42:24 +0900

    Move #search_from_groonga from top level to Epub::App
    In order to stop define method for controllers on top level context.

--- app/app.rb
+++ app/app.rb
@@ -58,5 +58,26 @@ module Epub
     #     render 'errors/505'
     #   end
     #
+
+    def search_from_groonga(query_words)
+      records = nil
+      options = {
+        protocol: :http,
+        host: settings.droonga_host,
+        port: settings.droonga_port,
+      }
+      Groonga::Client.open(options) do |client|
+        select = client.select(
+          :table => :Books,
+          :query => query_words,
+          :match_columns => 'author,title,main_text',
+          :output_columns => 'author,title,snippet_html(main_text)',
+          :command_version => 2,
+        )
+        records = select.records
+      end
+      records
+    end
   end
+
 end
--- app/controllers/main-controller.rb
+++ app/controllers/main-controller.rb
@@ -20,23 +20,3 @@ Epub::App.controllers do
   end
 
 end
-
-def search_from_groonga(query_words)
-  records = nil
-  options = {
-    protocol: :http,
-    host: settings.droonga_host,
-    port: settings.droonga_port,
-  }
-  Groonga::Client.open(options) do |client|
-    select = client.select(
-      :table => :Books,
-      :query => query_words,
-      :match_columns => 'author,title,main_text',
-      :output_columns => 'author,title,snippet_html(main_text)',
-      :command_version => 2,
-    )
-    records = select.records
-  end
-  records
-end
