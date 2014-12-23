edb7fe64a2b47017ba27b1f5446e59589482d908
KITAITI Makoto <KitaitiMakoto@gmail.com>
2014-12-23 09:02:42 +0900

    Add tests for search form

--- test/app/controllers/main_controller_test.rb
+++ test/app/controllers/main_controller_test.rb
@@ -5,10 +5,23 @@ class MainControllerTest < Test::Unit::TestCase
     assert last_response.ok?
   end
 
+  def test_get_index_form
+    visit '/'
+
+    assert page.has_selector?('form input[type="search"]')
+  end
+
   def test_get_index_query
     omit 'Background Droonga processes not running on Travis CI' if ENV['TRAVIS']
     get '/?q=test'
 
     assert last_response.ok?
+  end
+
+  def test_get_index_query_form
+    omit 'Background Droonga processes not running on Travis CI' if ENV['TRAVIS']
+    visit "/?q=%3C"
+
+    assert_equal '<', find('form input[type="search"]').value
   end
 end
